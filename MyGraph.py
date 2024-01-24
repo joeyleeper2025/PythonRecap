fc3982fd5aff2ac67842b12cbbe736d04a025aca
Joey <Joey_Leeper1@baylor.edu>
2024-01-24 13:29:23 -0600

    updated gitignore and changed print statement

--- MyGraph.py
+++ MyGraph.py
@@ -1,9 +1,9 @@
-import matplotlib.pyplot as plt
-import numpy as np
-
-x = np.linspace(0, 20, 100)
+import matplotlib.pyplot as plt  #install library - creating a virtual enviroment and installing into that enviroment 
+import numpy as np               #step 1 create virtual enviroment in terminal window py - 3 -m venv myvenv
+                                # activate your virtual enviroment 
+x = np.linspace(0, 20, 100)       #install third party module
 plt.plot(x, np.sin(x))
 
 plt.show()
 
-print("Hi there!")
+print("Hello World")
